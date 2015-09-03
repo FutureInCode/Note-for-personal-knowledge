0db63e56f2fb415cfe3b27252d9cc3f343aa9e92
ZhaoYao <2545933670@qq.com>
2015-09-03 22:09:49 +0800

    修改了之前存储混乱的情况，完成了文字内容的存储和删除，暂时不支持修改笔记内容的功能

--- js/personalNote.js
+++ js/personalNote.js
@@ -1,3 +1,4 @@
+
 window.onload = usingNote;
 function usingNote(){
 	var noteLabel = window.document.getElementById("noteLabel");
@@ -7,11 +8,10 @@ function usingNote(){
 	var classifications = window.document.getElementById("classifications");
 	//localStorage.clear();
 	loadContent();
-	// 新建分组
 	// 点击“新建分组”按钮出现编辑分组的界面
 	var newGroupButton = window.document.getElementById("groupButton");
 	newGroupButton.onclick = showGroupEdition;
-	// 编辑分组名，使用localStorage存储数据并完成，界面跳转至新建的分组界面
+	// 新建分组结束，点击完成按钮
 	var finishButtonList = window.document.getElementsByClassName("finishButt");
 	finishButtonList[1].onclick = finishGroupEdit;
 	// 点击分组的叉号按钮删除该分组及其组内的笔记
@@ -39,6 +39,27 @@ function usingNote(){
 		finishContentButton.addEventListener("click",finishContentEdit,false);
 	}else if(finishContentButton.attachEvent){
 		finishContentButton.attachEvent("onclick",finishContentEdit);
+	} 
+	//点击“取消”按钮取消编辑笔记
+	var cancelButtonList = window.document.getElementsByClassName("cancelButt");
+	var cancelContentButton = cancelButtonList[0];
+	if(cancelContentButton.addEventListener){
+		cancelContentButton.addEventListener("click",cancelEditContent,false);
+	}else if(cancelContentButton.attachEvent){
+			cancelContentButton.attachEvent("onclick",cancelEditContent);
+	}
+	//点击笔记的“删除”按钮
+	if(contentDisplay.addEventListener){
+		contentDisplay.addEventListener("click",deleteNote,false);
+	}else if(contentDisplay.attachEvent){
+		contentDisplay.attachEvent("onclick",deleteNote);
+	}
+	//点击笔记标题进入笔记
+	var notesList = window.document.getElementById("list");
+	if(notesList.addEventListener){
+		notesList.addEventListener("click",showNote,false);
+	}else if(notesList.attachEvent){
+		notesList.attachEvent("onclick",showNote);
 	}
 }
 //加载内容
@@ -46,30 +67,60 @@ function loadContent(){
 	if(window.document.cookie){
 		var cookieList = getCookie();
 		//页面刷新时保持当前content页面
+		keepCurrentPage(cookieList);
+		//加载当前的分组
+		loadCurrentGroupList();
+		//加载noteList的标题名字以及笔记列表
+		loadNoteList();
+		//加载展示笔记的内容
+		loadContentDisplay();
+	}
+}
+//加载当前的分组列表
+function loadCurrentGroupList(){
+	if(localStorage.groupList!=null){
+		var groupCode = "";
+		var localGroupList = JSON.parse(localStorage.groupList);
+		localGroupList.forEach(function(item,index,array){
+			groupCode += "<div class='classificationTd'>"
+						+item+"<button alt='删除分组'></button></div>";
+		});
+		classifications.innerHTML = groupCode;
+	}
+}
+//加载当前分组中的笔记列表
+function loadNoteList(){
+	if(localStorage.getItem(localStorage.currentGroup)!=null){
+		window.document.getElementsByTagName("h3")[0].innerHTML = localStorage.currentGroup;
+		var currentNotesList = JSON.parse(localStorage.getItem(localStorage.currentGroup));
+		var currentNoteListCode = "";
+		currentNotesList.forEach(function(item,index,array){
+			currentNoteListCode += "<p class='noteTitle'>"+item.name+"</p>"
+		});
+		currentNoteListCode += "<input type='button' value='新建笔记'></input>";
+		window.document.getElementById("list").innerHTML = currentNoteListCode;
+	}else
+		window.document.getElementById("list").innerHTML ="<input type='button' value='新建笔记'></input>";
+}
+//加载展示笔记页面的内容
+function loadContentDisplay(){
+		if(localStorage.currentGroup != null){
+			var newGroup = JSON.parse(localStorage.getItem(localStorage.currentGroup));
+			var filterList = newGroup.filter(function(item,idex,array){
+				if(item.name == localStorage.currentNoteName)return true;
+			});
+			var currentNote = filterList[0];
+			contentDisplay.innerHTML = "<section class='title'><h1>"+currentNote.name
+					+"</h1><button class='deleteButton'></button><textarea cols='146' rows='20' disabled='disabled'>"
+					+currentNote.content+"</textarea>";
+		}
+}
+//保持当前页面
+function keepCurrentPage(cookieList){
 		groupEdition.style.left = cookieList["currentGroupEditionLeft"];
 		noteContentEdition.style.left = cookieList[" currentNoteContentEditionLeft"];
 		contentDisplay.style.left = cookieList[" currentContentDisplayLeft"];	
 		noteLabel.style.left = cookieList[" currentNotelabelLeft"];
-		//加载当前的分组和笔记
-		/* if(window.document.getElementsByClassName("classificationTd")){
-			
-		} */
-		var insertStr = "";
-		for(var k=0;k<parseInt(localStorage.groupNum);k++){
-			if(localStorage.getItem("group"+k)!=null)
-				insertStr += localStorage.getItem("group"+k);
-		}
-		classifications.innerHTML = insertStr;//加载分组
-		//加载noteList的标题名字以及笔记列表
-		window.document.getElementsByTagName("h3")[0].innerHTML = localStorage.currentNoteListName;
-		if(localStorage.currentList!=null)
-		window.document.getElementById("list").innerHTML = localStorage.currentList;
-		//加载展示笔记的内容
-		var currentGroupName = window.document.getElementsByTagName("h3")[0];
-		var currentNoteNum = localStorage.getItem(currentGroupName+"noteNum");
-		contentDisplay.innerHTML = "<section class='title'><h1>"+localStorage.getItem("currentLabel")+"</h1><button class='modifyButton'></button><button class='deleteButton'></button></section>"
-									+localStorage.getItem(currentGroupName+"note"+currentNoteNum+"content");
-	}
 }
 //新建分组的处理程序
 function showGroupEdition(){
@@ -101,104 +152,138 @@ function getCookie(){
 function finishGroupEdit(){
 	var groupInput = window.document.getElementById("groupNameInput");
 	if(groupInput.value){
-		var num = window.document.getElementsByClassName("classificationTd").length;//获取当前分组的数目
-		classifications.innerHTML = "<div class='classificationTd' id='group"+num+"'>"+groupInput.value+"<button alt='删除分组'></button></div>";
-		//将创建的分组存储起来，groupList保存创各个分组
-		//localStorage.groupList += "<div class='classificationTd' id='group"+num+"'>"+groupInput.value+"<button alt='删除分组'></button></div>";
-		localStorage.setItem("group"+num,"<div class='classificationTd' id='group"+num+"'>"+groupInput.value+"<button alt='删除分组'></button></div>");
-		localStorage.groupNum = num+1;//记录当前的分组数
+		if(localStorage.groupList==null)var localGroupList = [];
+		else var localGroupList = JSON.parse(localStorage.groupList);
+		localGroupList[localGroupList.length] = groupInput.value;
+		//分组列表中存储新的分组名
+		save("groupList",localGroupList);
 		//将内容显示部分切换为分组的显示
 		var contentList = window.document.getElementsByClassName("noteContent");
 		for(var j=0;j<contentList.length;j++){
 			if(j==3){
 				contentList[j].style.left = "15%";
-				window.document.cookie = "currentNotelabelLeft=15%";
 			}else{
 				contentList[j].style.left = "2000px";
-				window.document.cookie = "currentNoteContentEditionLeft=2000px";
-				window.document.cookie = "currentGroupEditionLeft=2000px";
-				window.document.cookie = "currentContentDisplayLeft=2000px";				
 			}
 		}
+		window.document.cookie = "currentNotelabelLeft=15%";
+		window.document.cookie = "currentNoteContentEditionLeft=2000px";
+		window.document.cookie = "currentGroupEditionLeft=2000px";
+		window.document.cookie = "currentContentDisplayLeft=2000px";			
 		//单击“完成”后，加载当前的分组和笔记
-		window.document.getElementsByTagName("h3")[0].innerHTML = groupInput.value;
-		window.document.getElementById("list").innerHTML = "<input type='button' value='新建笔记'></input>";
-		var insertStr = "";
-			for(var k=0;k<parseInt(localStorage.groupNum);k++){
-				if(localStorage.getItem("group"+k)!=null)
-					insertStr += localStorage.getItem("group"+k);
-			}
-			classifications.innerHTML = insertStr;
+		localStorage.currentGroup = groupInput.value;//保存当前的分组名
+		window.document.getElementsByTagName("H3")[0].innerHTML = localStorage.currentGroup;
+		loadCurrentGroupList();
+		loadNoteList();
 	}
 }
-//删除指定分组以及其分组中的所有笔记（未完成删除相应分组的笔记）
+//将参数saveItem转化为字符串存在localStorage中的localStorageItem中去
+function save(localStorageItem,saveItem){
+	saveItem = JSON.stringify(saveItem);
+	localStorage.setItem(localStorageItem,saveItem);
+}
+//从分组列表中删除指定参数的分组
+function deleteFromGroupList(groupName){
+	var localGroupList = JSON.parse(localStorage.groupList);
+	localGroupList.forEach(function(item,index,array){
+		if(item == groupName)array.splice(index,1);
+	});
+	save("groupList",localGroupList);
+}
+//从相应分组的笔记列表中删除笔记
+function deleteFromNoteList(tarTitle,group){
+	var localGroup = JSON.parse(localStorage.getItem(group));
+	localGroup.forEach(function(item,index,array){
+		if(item.name == tarTitle)array.splice(index,1);
+	});
+	save(group,localGroup);
+}
+//获取点击的按钮的值
+function getTargetName(e){
+	if(navigator.appName.indexOf("Explorer")>-1)//IE下获取div中的文本
+		var tarName = e.target.innerText;
+	else 
+		var tarName = e.target.textContent;//其他浏览器下获取
+	return tarName;
+}
+//删除指定分组以及其分组中的所有笔记
 function deleteGroup(event){
 	if(event.target.tagName == "BUTTON"){
-		if(window.confirm("确定要删除这个分组以及其分组内的所有笔记？")==true){
+		if(window.confirm("确定要删除这个分组以及其分组内的所有笔记？")){
 			var deleteTarget = event.target.parentNode;
-			var deleteId = deleteTarget.id;
 			event.target.parentNode.parentNode.removeChild(deleteTarget);
 			if(navigator.appName.indexOf("Explorer")>-1)//IE下获取div中的文本
-				var delGroupName = deleteTarget.innerText;
+				var delGroupName = event.target.parentNode.innerText;
 			else 
-				var delGroupName = deleteTarget.textContent;//其他浏览器下获取
+				var delGroupName = event.target.parentNode.textContent;//其他浏览器下获取
 			var hName = window.document.getElementsByTagName("h3")[0];
-			if(delGroupName == hName.innerHTML)hName.innerHTML = "";
-			//删除localStorage中应该被删除的元素
-			localStorage.removeItem(deleteId);
+			if(delGroupName == hName.innerHTML){
+				hName.innerHTML = "";
+				//切换内容区为contentDisplay界面
+				noteContentEdition.style.left="2000px";
+				groupEdition.style.left="2000px";
+				contentDisplay.style.left="15%";
+				noteLabel.style.left="2000px";
+				window.document.cookie = "currentNoteContentEditionLeft=2000px";
+				window.document.cookie = "currentContentDisplayLeft=15%";
+				window.document.cookie = "currentGroupEditionLeft=2000px";
+				window.document.cookie = "currentNotelabelLeft=2000px";
+				contentDisplay.innerHTML = "<section class='title'><h1>欢迎使用web笔记本！</h1></section>";
+				localStorage.currentDisplayCode = "<section class='title'><h1>欢迎使用web笔记本！</h1></section>";
+				
+				localStorage.currentGroup = null;
+				localStorage.currentNoteName = null;
+			}
+			//删除localStorage的groupList中应该被删除的元素
+			deleteFromGroupList(delGroupName);
+			//删除分组内的所有笔记
+			var delGroup = JSON.parse(localStorage.getItem(delGroupName));
+			delGroup.splice(0,delGroup.length);
+			save(delGroupName,delGroup);
+			
+			loadCurrentGroupList();
+			loadNoteList();
 			//阻止事件冒泡
 			if(event.stopPropagation)event.stopPropagation();
 			cancelBubble = true;//IE
 		}
 	}
 }
-//点击进入相应分组的处理函数
+//点击进入相应分组
 function enterGroup(event){
-	if(navigator.appName.indexOf("Explorer")>-1)//IE下获取div中的文本
-		var groupName = event.target.innerText;
-	else 
-		var groupName = event.target.textContent;//其他浏览器下获取
 	if(event.target.className == "classificationTd"){
-		var groupStr = event.target.id;
-		//alert(localStorage.getItem(groupName+"realNoteNum"));
-		if(localStorage.getItem(groupName+"realNoteNum")!=null){
-			var noteNum = parseInt(localStorage.getItem(groupName+"noteNum"));
-			var list = window.document.getElementById("list");
-			var listStr = "";
-			//加载list中的笔记
-			for(var i1=0;i1<noteNum;i1++){
-				if(localStorage.getItem(groupName+"note"+i1)!=null)
-					listStr += localStorage.getItem(groupName+"note"+i1);
-			}
-			listStr += "<input type='button' value='新建笔记'></input>";
-			list.innerHTML = listStr;
-			localStorage.currentList = listStr;
+		var groupName = getTargetName(event);
+		localStorage.setItem("currentGroup",groupName);
+		if(localStorage.getItem(groupName)!=null){
+			loadCurrentGroupList();
+			loadNoteList();
 		}
-		if(localStorage.getItem(groupName+"realNoteNum")==null){
+		if(localStorage.getItem(groupName==null)){
 			var list = window.document.getElementById("list");
 			list.innerHTML = "<input type='button' value='新建笔记'></input>";
-			localStorage.currentList = "<input type='button' value='新建笔记'></input>";
+			localStorage.setItem("currentNoteTitleListCode","<input type='button' value='新建笔记'></input>");
 		}
 		//切换内容区
 		var contentList = window.document.getElementsByClassName("noteContent");
 		for(var j1=0;j1<contentList.length;j1++){
 			if(j1==3){
 				contentList[j1].style.left = "15%";
-				window.document.cookie = "currentNotelabelLeft=15%";
 			}else{
 				contentList[j1].style.left = "2000px";
-				window.document.cookie = "currentNoteContentEditionLeft=2000px";
-				window.document.cookie = "currentGroupEditionLeft=2000px";
-				window.document.cookie = "currentContentDisplayLeft=2000px";				
+		
 			}
 		}
+		window.document.cookie = "currentNotelabelLeft=15%";
+		window.document.cookie = "currentNoteContentEditionLeft=2000px";
+		window.document.cookie = "currentGroupEditionLeft=2000px";
+		window.document.cookie = "currentContentDisplayLeft=2000px";		
 		//笔记列表的标题相应改变
-		localStorage.currentNoteListName = groupName;
 		window.document.getElementsByTagName("h3")[0].innerHTML = groupName;
+		//取消冒泡
+		if(event.stopPropagation)event.stopPropagation();
+		event.cancelBubble = true;
 	}
-	//取消冒泡
-	if(event.stopPropagation)event.stopPropagation();
-	event.cancelBubble = true;
+
 }
 //创建笔记
 function createNote(event){
@@ -226,33 +311,20 @@ function finishContentEdit(event){
 	if(labelInput.value==""||noteEditArea.value=="")
 		alert("请输入标题或笔记内容！");
 	else{
-		var currentGroupName = window.document.getElementsByTagName("h3")[0].innerHTML;
+		var currentGroupName = localStorage.currentGroup;
 		//存储相应分组信息
-		localStorage.setItem(currentGroupName+"noteNum",window.document.getElementsByClassName("noteTitle").length);//保存当前分组的笔记数
-		var currentNoteNum = localStorage.getItem(currentGroupName+"noteNum");
-		localStorage.setItem(currentGroupName+"note"+currentNoteNum+"title",labelInput.value);
-		localStorage.setItem(currentGroupName+"note"+currentNoteNum,"<p class='noteTitle'>"+labelInput.value+"</p>");
-		localStorage.setItem(currentGroupName+"note"+currentNoteNum+"content",
-							"<textarea cols='146' rows='20' disabled='disabled' id='noteContentDisplay'>"
-								+noteEditArea.value+"</textarea>");
-		contentDisplay.innerHTML = "<section class='title'><h1>"+labelInput.value
-									+"</h1><button class='modifyButton'></button><button class='deleteButton'></button></section>"
-									+localStorage.getItem(currentGroupName+"note"+currentNoteNum+"content");
-		localStorage.setItem("currentLabel",labelInput.value);
-		alert(currentNoteNum);
-		currentNoteNum = parseInt(currentNoteNum)+1;
-		alert(currentNoteNum);
-		localStorage.setItem(currentGroupName+"noteNum",currentNoteNum);
-		localStorage.setItem(currentGroupName+"realNoteNum",currentNoteNum);//保存真实的笔记数，删除一个笔记时这个值要减1
-		//笔记列表中添加笔记的标题
-		var listStr = "";
-		for(var k2=0;k2<(window.document.getElementsByClassName("noteTitle").length+1);k2++){
-			if(localStorage.getItem(currentGroupName+"note"+k2)!=null)
-				listStr += localStorage.getItem(currentGroupName+"note"+k2);
-		}
-		listStr += "<input type='button' value='新建笔记'></input>";
-		window.document.getElementById("list").innerHTML = listStr;
-		localStorage.currentList = listStr;//存储当期笔记列表的代码一边刷新重新加载
+		localStorage.currentNoteName = labelInput.value;
+		var currentNoteList;
+		if(localStorage.getItem(currentGroupName)==null)currentNoteList = [];
+		else currentNoteList = JSON.parse(localStorage.getItem(currentGroupName));
+		var saveNote = {};
+		saveNote.name = labelInput.value;
+		saveNote.content = noteEditArea.value;
+		currentNoteList.push(saveNote);
+		save(currentGroupName,currentNoteList);
+		loadNoteList();
+		loadCurrentGroupList();
+		loadContentDisplay();
 		//切换内容区为内容展示
 		noteContentEdition.style.left="2000px";
 		groupEdition.style.left="2000px";
@@ -262,9 +334,71 @@ function finishContentEdit(event){
 		window.document.cookie = "currentContentDisplayLeft=15%";
 		window.document.cookie = "currentGroupEditionLeft=2000px";
 		window.document.cookie = "currentNotelabelLeft=2000px";
-		
 	}
 	//阻止事件冒泡
 	if(event.stopPropagation)event.stopPropagation();
 	cancelBubble = true;//IE
+}
+//点击“取消”按钮，如果是取消编辑分组则返回到笔记本的首页，如果是取消编辑笔记，则返回到分组首页
+function cancelEditContent(event){
+	//切换内容区为contentDisplay界面
+		noteContentEdition.style.left="2000px";
+		groupEdition.style.left="2000px";
+		contentDisplay.style.left="15%";
+		noteLabel.style.left="2000px";
+		window.document.cookie = "currentNoteContentEditionLeft=2000px";
+		window.document.cookie = "currentContentDisplayLeft=15%";
+		window.document.cookie = "currentGroupEditionLeft=2000px";
+		window.document.cookie = "currentNotelabelLeft=2000px";
+		//阻止事件冒泡
+		if(event.stopPropagation)event.stopPropagation();
+		cancelBubble = true;//IE
+}
+//删除指定笔记
+function deleteNote(event){
+	if(event.target.className == "deleteButton"){
+		if(confirm("确定删除笔记？")){
+			var noteTitle = localStorage.currentNoteName;
+			var currentGroupName = localStorage.currentGroup;
+			deleteFromNoteList(noteTitle,currentGroupName);
+			loadNoteList();
+			//切换内容区
+			var contentList = window.document.getElementsByClassName("noteContent");
+			for(var j1=0;j1<contentList.length;j1++){
+				if(j1==3){
+					contentList[j1].style.left = "15%";
+				}else{
+					contentList[j1].style.left = "2000px";
+				}
+			}
+			window.document.cookie = "currentNotelabelLeft=15%";
+			window.document.cookie = "currentNoteContentEditionLeft=2000px";
+			window.document.cookie = "currentGroupEditionLeft=2000px";
+			window.document.cookie = "currentContentDisplayLeft=2000px";			
+			contentDisplay.innerHTML = "<section class='title'><h1>欢迎使用web笔记本！</h1></section>";
+			localStorage.currentDisplayCode = "<section class='title'><h1>欢迎使用web笔记本！</h1></section>"; 
+		}
+	}
+	//阻止事件冒泡
+		if(event.stopPropagation)event.stopPropagation();
+		cancelBubble = true;//IE
+}
+//点击展示相应笔记
+function showNote(event){
+	if(event.target.className == "noteTitle"){
+		var currentNoteName = getTargetName(event);
+		localStorage.currentNoteName = currentNoteName;
+		loadContentDisplay();
+		noteContentEdition.style.left="2000px";
+		groupEdition.style.left="2000px";
+		contentDisplay.style.left="15%";
+		noteLabel.style.left="2000px";
+		window.document.cookie = "currentNoteContentEditionLeft=2000px";
+		window.document.cookie = "currentContentDisplayLeft=15%";
+		window.document.cookie = "currentGroupEditionLeft=2000px";
+		window.document.cookie = "currentNotelabelLeft=2000px";
+	}
+	//阻止事件冒泡
+		if(event.stopPropagation)event.stopPropagation();
+		cancelBubble = true;//IE
 }
